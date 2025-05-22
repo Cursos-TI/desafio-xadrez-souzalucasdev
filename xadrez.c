82fbca8b947086d4331eecfa56948c35d40a94aa
Lucas Souza <109295183+souzalucasdev@users.noreply.github.com>
2025-05-22 10:04:02 +0000

    Implementacao de funcoes recursivas

--- xadrez.c
+++ xadrez.c
@@ -4,64 +4,79 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    int bispoDiagonal = 0, rainhaEsquerda = 0, movimentoBaixo = 0;
 
-    //Torre - Movimento linear
-    for(int i=1; i<6; i++){
-        printf("Torre moveu uma casa para cima \n");
+//Criando uma funcao generica para qualquer movimento de qualquer peca em qualquer direcao (exceto cavalo);
+void movimento(int casas,  char *peca, char *direcao){
+    if(casas > 0){
+        printf("%s movimentou uma casa para %s \n", peca, direcao);
+        movimento(casas -1, peca, direcao);
     }
+}
 
-    //Bispo - Movimento diagonal
-    do {
-        printf("Bispo moveu uma casa na diagonal superior direita \n");
-        bispoDiagonal++;
-    } while(bispoDiagonal < 5);
-        
-
-    //Rainha - Movimento híbrido
-    while(rainhaEsquerda <= 8){
-        printf("Rainha moveu uma casa para a esquerda \n");
-        rainhaEsquerda++;
-    }
-
-    //Cavalo - Movimento em L
-    int i = 1;
+void movimentoCavalo(char *direcaoPrincipal, char *direcaoSecundaria){
+        int i = 1;
 
     while(i<=2){
-        printf("Cavalo moveu uma casa para baixo \n");
+        printf("Cavalo moveu uma casa para %s \n", direcaoPrincipal);
         i++;
 
         for(i; i>2;){
-            printf("Cavalo moveu uma casa para a esquerda \n");
+            printf("Cavalo moveu uma casa para %s \n", direcaoSecundaria);
             break;
         }
     }
 
+}
+
+int main() {
+
+    //Desafio Mestre - Funcao recursiva declarada no inicio do codigo(fora da funcao main())
+
+    //Movendo as peças com a funcao recursiva generica:
+    movimento(6, "Torre", "cima");
+    movimento(5, "Rainha", "direita");
+    movimento(4, "Bispo", "diagonal superior direita");
+
+    //Movendo o Cavalo com loops alinhados
+    movimentoCavalo("cima", "direita");
 
 
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    //**********************   Outros desafios: *********************
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // int bispoDiagonal = 0, rainhaEsquerda = 0, movimentoBaixo = 0;
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    //Torre - Movimento linear
+    // for(int i=1; i<6; i++){
+    //     printf("Torre moveu uma casa para cima \n");
+    // }
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    //Bispo - Movimento diagonal
+    // do {
+    //     printf("Bispo moveu uma casa na diagonal superior direita \n");
+    //     bispoDiagonal++;
+    // } while(bispoDiagonal < 5);
+        
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    //Rainha - Movimento híbrido
+    // while(rainhaEsquerda <= 8){
+    //     printf("Rainha moveu uma casa para a esquerda \n");
+    //     rainhaEsquerda++;
+    // }
+
+    //Cavalo - Movimento em L
+    // int i = 1;
+
+    // while(i<=2){
+    //     printf("Cavalo moveu uma casa para baixo \n");
+    //     i++;
+
+    //     for(i; i>2;){
+    //         printf("Cavalo moveu uma casa para a esquerda \n");
+    //         break;
+    //     }
+    // }
 
     return 0;
 }
